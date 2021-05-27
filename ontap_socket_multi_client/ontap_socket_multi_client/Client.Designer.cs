602954ea0cf6742aea04049e2111d6104256ef90
Nguyen Trien <trienvaj@gmail.com>
2021-05-27 12:41:46 +0700

    finish InterFace

---
+++ ontap_socket_multi_client/ontap_socket_multi_client/Client.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ontap_socket_multi_client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnKetNoi = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbTenSV = new System.Windows.Forms.ComboBox();
+            this.btnChapNhan = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnNopBaiThi = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnKetNoi);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 71);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập địa chỉ IP Server";
+            // 
+            // btnKetNoi
+            // 
+            this.btnKetNoi.Location = new System.Drawing.Point(281, 24);
+            this.btnKetNoi.Name = "btnKetNoi";
+            this.btnKetNoi.Size = new System.Drawing.Size(75, 23);
+            this.btnKetNoi.TabIndex = 2;
+            this.btnKetNoi.Text = "Kết Nối";
+            this.btnKetNoi.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server IP:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbTenSV);
+            this.groupBox2.Controls.Add(this.btnChapNhan);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(9, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(367, 88);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chọn tin Sinh viên";
+            // 
+            // cbTenSV
+            // 
+            this.cbTenSV.FormattingEnabled = true;
+            this.cbTenSV.Location = new System.Drawing.Point(27, 44);
+            this.cbTenSV.Name = "cbTenSV";
+            this.cbTenSV.Size = new System.Drawing.Size(246, 21);
+            this.cbTenSV.TabIndex = 3;
+            // 
+            // btnChapNhan
+            // 
+            this.btnChapNhan.Location = new System.Drawing.Point(284, 42);
+            this.btnChapNhan.Name = "btnChapNhan";
+            this.btnChapNhan.Size = new System.Drawing.Size(75, 23);
+            this.btnChapNhan.TabIndex = 2;
+            this.btnChapNhan.Text = "Chấp nhận";
+            this.btnChapNhan.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chọn Tên của minh sau đó nhãn vào nút Chấp Nhận";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 202);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(364, 173);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thông tin Sinh Viên";
+            // 
+            // btnNopBaiThi
+            // 
+            this.btnNopBaiThi.Location = new System.Drawing.Point(126, 381);
+            this.btnNopBaiThi.Name = "btnNopBaiThi";
+            this.btnNopBaiThi.Size = new System.Drawing.Size(138, 23);
+            this.btnNopBaiThi.TabIndex = 2;
+            this.btnNopBaiThi.Text = "Nộp bài thi";
+            this.btnNopBaiThi.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 146);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Thời gian Con lại:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Đề thi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Thời gian làm bài:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Môn khi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "MSSV:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Họ và Tên Sinh Viên:";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 416);
+            this.Controls.Add(this.btnNopBaiThi);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKetNoi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbTenSV;
+        private System.Windows.Forms.Button btnChapNhan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnNopBaiThi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ontap_socket_multi_client/ontap_socket_multi_client/Server.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ontap_socket_multi_client
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBatDauLamBaiThi = new System.Windows.Forms.Button();
+            this.btnLayDSCSDL = new System.Windows.Forms.Button();
+            this.btnSenMessToAll = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnNhapVungIP = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lsDeThi = new System.Windows.Forms.ListBox();
+            this.btnThemDeThi = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnChapNhan = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btnBatDauLamBaiThi);
+            this.groupBox1.Controls.Add(this.btnLayDSCSDL);
+            this.groupBox1.Controls.Add(this.btnSenMessToAll);
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnNhapVungIP);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 287);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(229, 349);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnBatDauLamBaiThi
+            // 
+            this.btnBatDauLamBaiThi.Location = new System.Drawing.Point(18, 223);
+            this.btnBatDauLamBaiThi.Name = "btnBatDauLamBaiThi";
+            this.btnBatDauLamBaiThi.Size = new System.Drawing.Size(142, 29);
+            this.btnBatDauLamBaiThi.TabIndex = 0;
+            this.btnBatDauLamBaiThi.Text = "Bắt Đầu Làm Bài";
+            this.btnBatDauLamBaiThi.UseVisualStyleBackColor = true;
+            // 
+            // btnLayDSCSDL
+            // 
+            this.btnLayDSCSDL.Location = new System.Drawing.Point(18, 174);
+            this.btnLayDSCSDL.Name = "btnLayDSCSDL";
+            this.btnLayDSCSDL.Size = new System.Drawing.Size(142, 29);
+            this.btnLayDSCSDL.TabIndex = 0;
+            this.btnLayDSCSDL.Text = "Kích hoạt Tất cả Client";
+            this.btnLayDSCSDL.UseVisualStyleBackColor = true;
+            // 
+            // btnSenMessToAll
+            // 
+            this.btnSenMessToAll.Location = new System.Drawing.Point(18, 120);
+            this.btnSenMessToAll.Name = "btnSenMessToAll";
+            this.btnSenMessToAll.Size = new System.Drawing.Size(142, 29);
+            this.btnSenMessToAll.TabIndex = 0;
+            this.btnSenMessToAll.Text = "Send Message To All";
+            this.btnSenMessToAll.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(18, 74);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(142, 29);
+            this.btnDisconnect.TabIndex = 0;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // btnNhapVungIP
+            // 
+            this.btnNhapVungIP.Location = new System.Drawing.Point(18, 29);
+            this.btnNhapVungIP.Name = "btnNhapVungIP";
+            this.btnNhapVungIP.Size = new System.Drawing.Size(142, 29);
+            this.btnNhapVungIP.TabIndex = 0;
+            this.btnNhapVungIP.Text = "Nhập vùng IP";
+            this.btnNhapVungIP.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 306);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(179, 119);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thời gian còn lại";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lsDeThi);
+            this.groupBox3.Controls.Add(this.btnThemDeThi);
+            this.groupBox3.Location = new System.Drawing.Point(197, 319);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(318, 119);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chọn Đề Thi";
+            // 
+            // lsDeThi
+            // 
+            this.lsDeThi.FormattingEnabled = true;
+            this.lsDeThi.Location = new System.Drawing.Point(6, 18);
+            this.lsDeThi.Name = "lsDeThi";
+            this.lsDeThi.Size = new System.Drawing.Size(167, 95);
+            this.lsDeThi.TabIndex = 1;
+            // 
+            // btnThemDeThi
+            // 
+            this.btnThemDeThi.Location = new System.Drawing.Point(178, 43);
+            this.btnThemDeThi.Name = "btnThemDeThi";
+            this.btnThemDeThi.Size = new System.Drawing.Size(104, 29);
+            this.btnThemDeThi.TabIndex = 0;
+            this.btnThemDeThi.Text = "Thêm Đề thi";
+            this.btnThemDeThi.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Controls.Add(this.btnChapNhan);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Location = new System.Drawing.Point(586, 319);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(259, 119);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Chọn môn thi và thời gian làm bài thi";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // btnChapNhan
+            // 
+            this.btnChapNhan.Location = new System.Drawing.Point(69, 77);
+            this.btnChapNhan.Name = "btnChapNhan";
+            this.btnChapNhan.Size = new System.Drawing.Size(104, 29);
+            this.btnChapNhan.TabIndex = 0;
+            this.btnChapNhan.Text = "Chấp nhận";
+            this.btnChapNhan.UseVisualStyleBackColor = true;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Server";
+            this.Text = "Serve";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBatDauLamBaiThi;
+        private System.Windows.Forms.Button btnLayDSCSDL;
+        private System.Windows.Forms.Button btnSenMessToAll;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnNhapVungIP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lsDeThi;
+        private System.Windows.Forms.Button btnThemDeThi;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnChapNhan;
+    }
+}
+
